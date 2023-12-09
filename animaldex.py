9171c728ec5bafd6bb301552ec60c0c446a727e0
billygoat215 <150048640+billygoat215@users.noreply.github.com>
2023-12-08 22:28:05 -0500

    Update animaldex.py

--- animaldex.py
+++ animaldex.py
@@ -7,20 +7,20 @@ screen_height = 600
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-image = pygame.image.load('pokedex.png')
+image = pygame.image.load('pokedex2.png')
 
-def Background_sky(image):
-	size = pygame.transform.scale(image, (screen_width, screen_height))
-	screen.blit(size, (0,0))
+def draw_background(image):
+    size = pygame.transform.scale(image, (screen_width, screen_height))
+    screen.blit(size, (0, 0))
 
 run = True
 while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			run = False
-
-screen.fill((0, 255, 0))
-Background_sky(image)
+    screen.fill((0, 255, 0))
+    draw_background(image)
+    pygame.display.flip()
 
 pygame.quit()
