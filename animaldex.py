b1f5d9f97b3deafc8b3511b2a91c479c3f147b3c
billygoat215 <150048640+billygoat215@users.noreply.github.com>
2023-12-08 22:56:53 -0500

    Update animaldex.py

--- animaldex.py
+++ animaldex.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 pygame.init()
 
@@ -6,21 +7,38 @@ screen_width = 800
 screen_height = 600
 
 screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Start Menu')
 
-image = pygame.image.load('pokedex2.png')
+def draw_button():
+    button_width = 200
+    button_height = 50
+    button_color = (255, 0, 0)
+    button_x = (screen_width - button_width) // 2
+    button_y = (screen_height - button_height) // 2
 
-def draw_background(image):
-    size = pygame.transform.scale(image, (screen_width, screen_height))
-    screen.blit(size, (0, 0))
+    pygame.draw.rect(screen, button_color, (button_x, button_y, button_width, button_height))
 
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+    return pygame.Rect(button_x, button_y, button_width, button_height)
 
-    screen.fill((0, 255, 0))
-    draw_background(image)
-    pygame.display.flip()
+def main_menu():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if button_rect.collidepoint(event.pos):
+                    next_page()
 
-pygame.quit()
+        screen.fill((0, 255, 0))
+        button_rect = draw_button()
+
+        pygame.display.flip()
+
+def next_page():
+	screen.fill((0, 0, 255))
+	pygame.display.flip()
+	pygame.time.delay(1000000)
+
+if __name__ == "__main__":
+	main_menu()
